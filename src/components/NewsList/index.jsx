4348e1aede6d527609b8ee6253654f86ea32c3e7
Joe Mancera <joeharrymancera@gmail.com>
2021-12-19 10:11:10 -0500

    Implemented NearScreen hook on newslist, remuve frome home becouse can't set page here

--- src/components/NewsList/index.jsx
+++ src/components/NewsList/index.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
+import useNearScreen from "hooks/useNearScreen";
+import debounce from "just-debounce-it";
 import { useHackNews } from "hooks/useHackNews";
 import CardNews from "components/CardNews";
 import "./NewList.css";
 
 export default function NewsList() {
   const { newsList } = useHackNews({ keyword: "reactjs" });
+  const { isNearScreen, finalPageDivRef } = useNearScreen({ firstTime: false });
+
+  const debounceHandleNextPage = useCallback(
+    debounce(() => console.log("Next page", isNearScreen), 500),
+    [isNearScreen]
+  );
+
+  useEffect(() => {
+    if (isNearScreen) debounceHandleNextPage();
+  }, [isNearScreen, debounceHandleNextPage]);
+
   return (
     <div className="list-news">
       {newsList.map((news) => (
         <CardNews newsInfo={news} key={`${news.story_id}-${news.created_at}`} />
       ))}
+      <div id="finalPageDivRef" ref={finalPageDivRef}></div>
     </div>
   );
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,33 +1,10 @@
-import React, { useRef, useState, useEffect } from "react";
+import React from "react";
 import NewsList from "components/NewsList";
 
 export default function Home() {
-  const finalPageDiv = useRef();
-  const [isNearFinalElement, setIsNearFinalElement] = useState(false);
-
-  useEffect(() => {
-    let callbackOnChange = (entries, observer) => {
-      const element = entries[0];
-      if (element.isIntersecting) {
-        setIsNearFinalElement(true);
-      } else {
-        setIsNearFinalElement(false);
-      }
-      console.log(isNearFinalElement);
-    };
-
-    const observer = new IntersectionObserver(callbackOnChange, {
-      rootMargin: "100px",
-    });
-    observer.observe(finalPageDiv.current);
-
-    return () => observer.disconnect();
-  }, [isNearFinalElement]);
-
   return (
     <>
       <NewsList />
-      <div id="finalPageDiv" ref={finalPageDiv}></div>
     </>
   );
 }
